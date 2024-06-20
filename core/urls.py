ca72705a3722db7548faaa8fd0deb1c604a3f623
Abdulwahid <abusabrina2158@gmail.com>
2024-06-20 15:18:21 +0300

    upaid

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.payment_list, name='payment_list'),
+    path('unpaid/', views.tenants_unpaid, name='tenants_unpaid'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Payment
+from .models import Payment, Tenant
 from datetime import datetime
 
 def payment_list(request):
@@ -17,6 +17,28 @@ def payment_list(request):
 
     return render(request, 'core/payment_list.html', {
         'payments': payments,
+        'current_year': current_year,
+        'years': years,
+        'months': months
+    })
+
+
+def tenants_unpaid(request):
+    month = request.GET.get('month')
+    year = request.GET.get('year')
+    
+    if month and year:
+        paid_tenants = Payment.objects.filter(payment_date__year=year, payment_date__month=month).values_list('tenant', flat=True)
+        unpaid_tenants = Tenant.objects.exclude(id__in=paid_tenants)
+    else:
+        unpaid_tenants = Tenant.objects.all()
+    
+    current_year = datetime.now().year
+    years = range(2020, current_year + 1)
+    months = range(1, 13)
+
+    return render(request, 'core/tenants_unpaid.html', {
+        'unpaid_tenants': unpaid_tenants,
         'current_year': current_year,
         'years': years,
         'months': months
